6c684320f4be808f806c39089bf0e9d13838a747
Александр Дрожкин <Santay2009@yandex.ru>
2020-06-06 15:03:24 +0300

    CreateProject

---
+++ WpfApp1/View/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace WpfApp1.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WpfApp1.App app = WpfApp1.App.Current as App;
+            app.InitializeComponent();
+        }
+    }
+}
